3d86538cbf79a64cdcfb03ff25c06dc7e86abbd9
Vince Mendella <vmendella@gmail.com>
2023-04-14 15:04:54 -0500

    add function.php, screenshot.png. Add and insert code into header.php and footer.php

---
+++ footer.php
@@ -0,0 +1,55 @@
+</main>
+
+<!-- FOOTER
+=================================================== -->
+  <footer class="footer">
+    <div class="container footer__nav">
+      <div class="footer__branding">
+        <div class="footer__logo">
+          <img src="img/CommercialSalesLogo_244x54.svg" alt="" width="275">
+        </div>
+        <div class="footer__contact">
+          <a href="tel:204-934-1471">Phone: (204) 934.1471</a><br>
+          <a href="fax:204-934-1685">Fax Order: (204) 934.1685</a><br>
+          Email: <a href="mailto:contractsales@northwest.ca">contractsales@northwest.ca</a><br>
+          77 Main Street, Winnipeg MB  R3C 2R1
+        </div>
+      </div>
+      <nav class="footer__menu" aria-label="Footer">
+        <ul class="footer__menu--navigation">
+          <li><a href="about.html">About</a></li>
+          <li>Products
+            <ul class="footer__menu--submenu">
+              <li><a href="home_furnishings.html">Home Furnishings & Appliances</a></li>
+              <li><a href="#">Office Supplies & Furniture</a></li>
+              <li><a href="#">Cleaning & Janitorial Supplies</a></li>
+              <li><a href="#">School Supplies & Sporting Equipment</a></li>
+              <li><a href="#">Housewares</a></li>
+              <li><a href="#">Lumber & Building Supplies</a></li>
+              <li><a href="#">Bulk Food Orders</li>
+              <li><a href="#">Transportation Vehicles</a></li>
+              <li><a href="#">Community Events & Fireworks</a></li>
+            </ul>
+          </li>
+          <button class="footer__item--btn"><a class="button btn btn--dark" href="contact.html">Contact</a></button>
+          <!-- <li><a href="contact.html">Contact</a></li> -->
+        </ul>
+
+      </nav>
+    </div> <!-- container -->
+    <div class="container">
+      <div class="site__info">
+        <div>
+          <p class="copyright">Copyright © 2021 The North West Company</p>
+        </div>
+        <div class="legal">
+          <a href="#" target="_blank">Privacy</a> | <a href="#" target="_blank">Terms & Conditions</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+<!-- ============================================== -->
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Commercial Sales - Home</title>
+
+  <meta property="og:title" content="Commercial Sales - northerncommercial.ca">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://northerncommercial.ca/">
+  <meta property="og:image" content="">
+
+  <link rel="shortcut icon" type="image/jpg" href="favicon.ico"/>
+
+  <!-- Google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Georama:wght@400;700;900&family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
+
+  <link rel="stylesheet" href="dist/style.css">
+  <script defer src="dist/script.js"></script>
+
+  <?php wp_head(); ?>
+
+</head>
+
+<body>
+
+    <!-- HEADER / NAVIGATION SECTION START
+    =================================================== -->
+    <header class="header masthead">
+      <div class="overlay has-fade"></div>
+      
+      <div class="container header__section row">
+        <nav class="nav" aria-label="Site">
+          <div class="header__logo site-branding">
+            <a href="index.html"><img src="img/CommercialSalesLogo_244x54_rev.svg" alt="Commercial Sales logo" width="225"></a>
+          </div> <!-- header__logo -->
+            
+            <a id="btnHamburger" class="nav__toggle header__menu hide-for-desktop" href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+            </a> <!-- nav__toggle header__menu hide-for-desktop -->
+            
+              <ul class="menu nav__list mobileMenu hide-for-mobile" role="list">
+                <li class="nav__item" role="presentation"><a role="menuitem" href="about.html">About</a></li> <!-- nav__item -->
+                <li class="nav__item has-submenu"><a tabindex="0">Products <span class="chevron-down">&#8964;</span></a>
+                  <ul class="submenu">
+                    <li class="subitem"><a role="menuitem" href="home_furnishings.html">Home Furnishings & Appliances</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">Office Supplies & Furniture</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">Cleaning & Janitorial Supplies</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">School Supplies & Sporting Equipment</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">Housewares</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">Lumber & Building Supplies</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">Bulk Food Orders</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">Transportation Vehicles</a></li>
+                    <li class="subitem"><a role="menuitem" href="#">Community Events & Fireworks</a></li>
+                  </ul>
+                </li> <!-- nav__item has-submenu -->
+
+                <button class="nav__item--btn"><a class="button btn btn--dark" href="contact.html" role="button">Contact</a></button>
+              </ul> <!-- nav__list header__links hide-for-mobile mobileMenu -->
+
+        </nav> <!-- nav -->
+      </div> <!-- container header__section -->
+
+    </header> <!-- HEADER / NAVIGATION SECTION END -->
+    <!-- ============================================== -->
+
+    <main>
