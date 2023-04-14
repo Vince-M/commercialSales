2c0bdff12f0b648184cb8fccba3db296439d048d
Vince Mendella <vmendella@gmail.com>
2023-04-14 15:07:18 -0500

    add .gitignore, index.php and style.css files

---
+++ index.php
@@ -0,0 +1,201 @@
+  <?php get_header(); ?>
+
+      <!-- HERO
+      =================================================== -->
+      <section class="hero feature" aria-labelledby="hero_section">
+
+          <div class="hero__index" style="
+            background: url('img/HERO_index_1920x1080-min.jpg') no-repeat;
+            background-position: top;
+            background-size: cover;" >
+                  <div class="hero__index--headline">
+                    <h1 class="hero__index--headline">We're Here to Help!</h1>
+                    <h4 class="hero__index--subhead">Delivering large-scale orders to your Community.</h4>
+                    <a href="contact.html"><button class="button btn btn--yellow">Find Out How</button></a>
+
+                    <section class="header-down-arrow">
+                      <img src="img/downarrow.svg" width="50" alt="">
+                    </section> <!-- header-down-arrow -->
+                  </div>
+          </div> <!-- hero__index -->
+      </section> <!-- hero feature -->
+      <!-- ============================================== -->
+
+      <!-- BULK ORDER
+      =================================================== -->
+        <section class="index__bulkOrder container">
+          <h3 class="bulk__header">Taking care of your bulk order needs</h3>
+          <p class="page__intro">
+            The Commercial Sales Division at NWC offers value pricing on your large scale orders. As logistical experts in the North, we pride ourselves on delivering a huge selection of goods to remote and rural communities.
+          </p>
+        </section> <!-- index__bulkOrder container -->
+      <!-- ============================================== -->
+
+      <!-- PRODUCTS
+      =================================================== -->
+        <section class="indexProducts index__fullWidth">
+          <div class="container index__fullWidth--text">
+            <h2 class="index__fullWidth--head">Products</h2>
+            <h5 class="index__fullWidth--subHead">From Groceries to General Merchandise.</h5>
+            <h6 class="index__fullWidth--subText">Get in touch with Commercial Sales to see what products best fit your needs</h6>
+            <a href="contact.html"><button class="button btn btn--white">Contact</button></a>
+          </div> <!-- container index__fullWidth--text -->
+        </section> <!-- indexProducts index__fullWidth -->
+      <!-- ============================================== -->
+
+      <!-- PRODUCT GRID
+      =================================================== -->
+        <section class="product__cards container" aria-labelledby="product_cards">
+          
+          <a href="home_furnishings.html">
+            <div class="product__card product__card1">
+              <div class="product__card--content">
+                  <div class="product__card--image">
+                      <img src="img/icon_homeFurnishings_82x69.svg" alt="Products - Home Furnishings and Appliances" width="82" height="69">
+                  </div>
+                  <p>home furnishings & appliances</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+          <a href="#">
+            <div class="product__card product__card2">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_officeSupplies_82x69.svg" alt="Products - Office Supplies and Furniture" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>office supplies & furniture</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+          <a href="#">
+            <div class="product__card product__card3">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_cleaningVaccum_82x69.svg" alt="Products - Cleaning and Janitorial Supplies" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>cleaning & janitorial supplies</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+          <a href="#">
+            <div class="product__card product__card4">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_schoolBook_82x69.svg" alt="Products - School Supplies and Sporting Equipment" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>school supplies & sporting equipment</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+          <a href="#">
+            <div class="product__card product__card5">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_housewaresPot_82x69.svg" alt="Products - Housewares" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>housewares</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+          <a href="#">
+            <div class="product__card product__card6">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_lumberSaw_82x69.svg" alt="Products - Lumber and Building Supplies" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>lumber & building supplies</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+          <a href="#">
+            <div class="product__card product__card7">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_bulkOrderFoods_82x69.svg" alt="Products - Bulk Food Orders" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>bulk food orders</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+          <a href="#">
+            <div class="product__card product__card8">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_transportationSUV_82x69.svg" alt="Products - Transportation" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>transportation vehicles</p>
+              </div> <!-- product__card--content -->
+            </div> <!-- product__card -->
+          </a>
+
+
+
+          <a href="#" class="product__card9">
+            <div class="product__card">
+              <div class="product__card--content">
+                <div class="product__card--image">
+                  <img src="img/icon_fireworks_82x69.svg" alt="Products - Community Events and Fireworks" width="82" height="69">
+                </div> <!-- product__card--image -->
+                <p>community events & fireworks</p>
+              </div> <!-- product__card--content -->
+            </div><!-- product__card -->
+          </a>
+        </section> <!-- product__cards container -->
+      <!-- ============================================== -->
+
+      <!-- SOLUTIONS
+      =================================================== -->
+        <section class="indexSolutions index_fullWidth">
+          <div class="solutionsText">
+            <div class="container index__fullWidth--solutions">
+              <h2 class="indexSolutions__head">Solutions</h2>
+              <h5 class="indexSolutions__subHead">300+ years of experience shipping products to the north.</h5>
+              <h6 class="indexSolutions__subText">Reach out to the Commercial Sales team, see what solution would be most suitable for you.</h6>
+              <button class="button btn btn--dark"><a href="contact.html" class="btn__contact">Contact</a></button>
+            </div> <!-- ourCustomers__pullOut--text -->
+            <div class="indexSolutions__background"></div> <!-- ourCustomers__pullOut -->
+          </div><!-- solutionsText -->
+        </section> <!-- indexSolutions index_fullWidth -->
+      <!-- ============================================== -->
+
+      <!-- TRANSPORT
+      =================================================== -->
+        <section class="transport container row">
+          <div class="air">
+            <figure>
+              <img src="img/transport_air_600x400-min.jpg" alt="" width="600">
+              <figcaption class="transport__caption">During the spring and summer months, we transport our goods by air and also sealift.</figcaption>
+            </figure>
+          </div>
+          <div class="iceRoad">
+            <figure>
+              <img src="img/transport_iceRoad_600x400-min.jpg" alt="" width="600">
+              <figcaption class="transport__caption">For reliable delivery of items during winter, we use a combination of air shipping and ice roads transportation.</figcaption>
+            </figure>
+          </div>
+        </section>
+      <!-- ============================================== -->
+
+      <!-- CTA SECTION
+      =================================================== -->
+        <section class="cta">
+          <div class="cta__text container row">
+            <h3 class="cta__pullBanner">Learn how Commercial Sales can be the perfect choice for all your community requirements.</h3>
+            <a href="contact.html"><button class="button btn btn--white">Get in Touch</button></a>
+          </div>
+        </section>
+      <!-- ============================================== -->
+
+      <!-- WAREHOUSE
+      =================================================== -->
+
+      <!-- ============================================== -->
+   
+    <?php get_footer(); ?>
