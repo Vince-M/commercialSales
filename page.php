52614db76e79ed6ec06df8f9a7cffff42b1759c2
Vince Mendella <vmendella@gmail.com>
2023-05-01 15:26:32 -0500

    add product head and subhead to page.php

--- page.php
+++ page.php
@@ -9,27 +9,34 @@
 
     <!-- PAGE INTRO
     =================================================== -->
-    <section class="pageIntro">
-        <div class="page__headBkgd">
-          <h1 class="page__heading container"><?php the_title(); ?></h1>
-        </div>
+      <section class="pageIntro">
+          <div class="page__headBkgd">
+            <h1 class="page__heading container"><?php the_title(); ?></h1>
+          </div>
 
-      <!-- CONTENT
-      =================================================== -->
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <!-- CONTENT
+        =================================================== -->
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <?php the_content(); ?>
+          <?php the_content(); ?>
 
-        <?php endwhile; else: ?>
+          <?php endwhile; else: ?>
 
-        <p>Sorry, no pages found</p>
+          <p>Sorry, no pages found</p>
 
-        <?php endif; ?>
+          <?php endif; ?>
 
       </section> <!-- pageIntro -->
 
+
+      <!-- PRODUCT GRID
+      =================================================== -->
+      <div class="productCardHead container row">
+        <h3>We offer quality products, at wholesale prices!</h3>
+        <h5>Our team of dedicated individuals are here to help source, procure and ship your requirements into communities across Northern Canada, year round!</h5>
+      </div>
+
       <section class="product__cards container" aria-labelledby="product_cards">
-      
         <?php
           $productCards = new WP_Query(array(
             'posts_per_page'    =>  -1,
