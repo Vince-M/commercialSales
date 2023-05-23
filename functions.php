b9bc234ad3531e1cab1985c0deaa158960387760
Vince Mendella <vmendella@gmail.com>
2023-05-23 11:19:04 -0500

    add style.css to functions and move <head> abovve <meta> tags.

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ function commercialsales_files() {
   wp_enqueue_script('commercialsales_main', get_theme_file_uri('/dist/script.js'), NULL, '1.0', true);
   wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Georama:wght@400;700;900&family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swa', [], null);
   wp_enqueue_style('commercialsales_main_styles',get_theme_file_uri('main.css'));
+  wp_enqueue_style('commercialsales_style',get_theme_file_uri('style.css'));
 }
 
 add_action( 'wp_enqueue_scripts', 'commercialsales_files' );
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<meta charset="<?php bloginfo('charset'); ?>">
 <head>
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/favicon.ico">
 
